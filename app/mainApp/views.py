2e36fe8f8bf0ead7ec2b382d37a1f6f0f0dc0b89
Christian Asch <christian.asch4@gmail.com>
2020-11-19 14:46:23 -0500

    Se agrega un mecanismo para que las páginas de la aplicación sólo puedan ser vistas por usuarios que han hecho login

--- app/mainApp/views.py
+++ app/mainApp/views.py
@@ -1,29 +1,36 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
 #from serviciosApp.models import Servicio
 from mainApp.models import Servicio, Noticia, Recurso
 
 # Create your views here.
 
+@login_required
 def inicio (request):
     #    return HttpResponse("Inicio")
     return render(request,"mainApp/inicio.html")
 
+@login_required
 def acerca (request):
     return render(request,"mainApp/acerca.html")
 
+@login_required
 def servicios (request):
     servicios = Servicio.objects.all()
 #    return render(request,"servicios/servicios.html", {"servicios": servicios})
     return render(request,"mainApp/servicios.html", {"servicios": servicios})
 
+@login_required
 def noticias (request):
     noticias = Noticia.objects.all()
     return render(request,"mainApp/noticias.html", {"noticias": noticias})
 
+@login_required
 def recursos (request):
     recursos = Recurso.objects.all()
     return render(request,"mainApp/recursos.html", {"recursos": recursos})
 
+@login_required
 def contacto (request):
 #    return HttpResponse("Contacto")
     return render(request,"mainApp/contacto.html")
